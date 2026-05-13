6b53e15eb36da4b61d5536361951612461843df9
Sumit282698 <126954034+Sumit282698@users.noreply.github.com>
2026-05-13 17:29:57 +0530

    Some Changes

--- app/src/main/java/com/sumit/muzixx/MusicViewModel.kt
+++ app/src/main/java/com/sumit/muzixx/MusicViewModel.kt
@@ -4,8 +4,6 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 
 class MusicViewModel : ViewModel() {
-    // This is a special list that tells the UI to refresh
-    // automatically when a song is added or removed.
     val songs = mutableStateListOf<Song>()
 
     fun loadSongs(songList: List<Song>) {
